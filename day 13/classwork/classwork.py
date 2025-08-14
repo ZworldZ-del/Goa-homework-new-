b3b04124648637dbd2d0f88ed373a6b4a491510b
ZworldZ-del <chagelishvilinikolozi@gmail.com>
2025-08-14 15:56:30 +0400

    second commit

---
+++ day 13/classwork/classwork.py
@@ -0,0 +1,6 @@
+for i in range(50):  # Loop
+    if i % 2 == 0:
+        print("ლუწია" + str(i))
+    else:
+        print("კენტია" +  str(i))        
+        # ---------------------------------
---
+++ day 13/homework/homework.py
@@ -0,0 +1,43 @@
+# level 13
+
+for i in range(50):  # Loop
+    if i % 2 == 0
+        print("დადებითი" + str(""i))
+    else:
+        print("უარყოფითი" +  str(""i))
+
+# --------------------------------------------
+
+for i in range(50):  # Loop
+    if i % 2 == 0
+        print("ლუწია" + str(""i))
+    else:
+        print("კენტია" +  str(""i))
+
+# ---------------------------------------------
+
+for i in range(50):  # Loop
+    if i => 100 
+        print("თქვენ ქულა მიღეთ" + str(""i))
+    else:
+        print("თქვენ ქულა ვერ მიღე და წაგე" +  str(""i))   
+
+# ---------------------------------------------
+
+even_numbers = (2, 4, 6, 8, 10)
+
+# 5 კენტი რიცხვი
+odd_number = (1, 3, 5, 7, 9)
+
+print("ლუწი რიცხვები:")
+for i in even_number:
+    print(i)
+
+print("კენტი რიცხვები:")
+for i in odd_number:
+    print(i)
+
+# ---------------------------------------------
+
+for i in range(6):  # Loop
+print(i)
