328fd782281888f856f3f8bd2fab499c59047aff
ZworldZ-del <chagelishvilinikolozi@gmail.com>
2025-08-18 15:16:37 +0400

    second commit

--- day 13/homework/homework.py
+++ day 13/homework/homework.py
@@ -1,10 +1,11 @@
 # level 13
 
+
 for i in range(50):  # Loop
     if i % 2 == 0
         print("დადებითი" + str(""i))
     else:
-        print("უარყოფითი" +  str(""i))
+        print("უარყოფითი" + str(""i))
 
 # --------------------------------------------
 
@@ -12,32 +13,33 @@ for i in range(50):  # Loop
     if i % 2 == 0
         print("ლუწია" + str(""i))
     else:
-        print("კენტია" +  str(""i))
+        print("კენტია" + str(""i))
 
 # ---------------------------------------------
 
 for i in range(50):  # Loop
-    if i => 100 
+    if i % 1 > 100
         print("თქვენ ქულა მიღეთ" + str(""i))
     else:
-        print("თქვენ ქულა ვერ მიღე და წაგე" +  str(""i))   
+        print("თქვენ ქულა ვერ მიღე და წაგე" + str(""i))   
 
 # ---------------------------------------------
 
 even_numbers = (2, 4, 6, 8, 10)
 
-# 5 კენტი რიცხვი
 odd_number = (1, 3, 5, 7, 9)
 
 print("ლუწი რიცხვები:")
-for i in even_number:
+
+for i in even_numbers:
     print(i)
 
 print("კენტი რიცხვები:")
+
 for i in odd_number:
     print(i)
 
 # ---------------------------------------------
 
-for i in range(6):  # Loop
+for i in range(50)
 print(i)
