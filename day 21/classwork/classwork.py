b66f4a21211a02459b6fefccfe2d01e01f99e83e
ZworldZ-del <chagelishvilinikolozi@gmail.com>
2025-09-14 16:09:58 +0400

    second commit

---
+++ day 21/classwork/classwork.py
@@ -0,0 +1,10 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, True, False, "string" ]
+print(len(array))
+# --------------------------------
+list = [10, 20, 30, 40, 50]
+for i in range(len(list)):
+    print(list[i])
+
+arry_lomi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+uime = arry_lomi[::-1]
+print(uime)
