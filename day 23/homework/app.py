a6d969a04ea15f29419adacf194db6211a833dc9
ZworldZ-del <chagelishvilinikolozi@gmail.com>
2025-09-19 19:35:34 +0400

    second commit

---
+++ day 23/homework/app.py
@@ -0,0 +1,14 @@
+for i in range(22, 105):   
+    if i % 2 == 0:         
+        print(i)
+
+for i in range(204, 427):  
+    if i % 2 != 0:         
+        print(i)
+
+age = int(input("Enter Your Age : "))
+
+if age < 18:
+    print("YOUR UNDERAGED YOU SHOULD BE 18")
+else:
+    print("YOU ARE GOOD TO GO")
