5e964a77fff27328a0ffe2d18d1b5fd99150d6dc
nikoloz chagelishvili <chagelishvilinikolozi@gmail.com>
2025-10-03 21:05:24 +0400

    second commit

---
+++ day 27/classwork/app.py
@@ -0,0 +1,7 @@
+# explain github desktop further and working on group work creating group adding members deciding about who will be leader
+
+newlist = ["capucino" , "dinosavri"]
+
+for i in range(101):
+    newlist.append(i)
+    print(newlist)
