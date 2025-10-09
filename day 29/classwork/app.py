7dad6fc21b3d28280f6711ceab94f5683d595ae1
nikoloz chagelishvili <chagelishvilinikolozi@gmail.com>
2025-10-09 18:03:13 +0400

    second commit

---
+++ day 29/classwork/app.py
@@ -0,0 +1,17 @@
+array = [1, 2, 3, 3, 4, 5, 6, 7, 9,9,9,9,9,9,11,11,12,12]
+array_count = array.count(11)
+array_index = array.index(9)
+
+print(array_count)
+print(array_index)
+# --------------------------------------------
+def lomi():
+    return "i am lomi"  # joke
+lomi()
+
+print(lomi())
+# --------------------------------------------
+
+def summer_func(a, b):
+    return a + b
+print(summer_func(6, 7))
---
+++ day 29/homework/app.py
@@ -0,0 +1,67 @@
+# 1)
+lion = [4 , 5 , 5 , 4 , 5]
+lion_count = lion.count(5)
+lion_index = lion.index(4)
+
+print(lion_count)
+print(lion_index)
+
+# ---------------------------------
+
+lion = ["dino" , "lino" , "baino" , "lion" , "dino"]
+lion_count = lion.count("daino")
+lion_index = lion.index("lino")
+
+print(lion_count)
+print(lion_index)
+
+# ---------------------------------
+
+def lion():
+    return "მე ვარ ლომი ნიკოლოზი" 
+lion()
+
+print(lion())
+
+# ---------------------------------
+
+
+def lion4():
+    numbers = []
+    for i in range(22):  
+        if i % 2 != 0:  
+            numbers.append(i)  
+    print(numbers)  
+print(lion4())
+
+# ---------------------------------------------
+
+def lion5():
+    სწორი_პასვორდი= "lomivarlomi!"
+    password = input("შეიყვანე პაროლი")
+    while password != სწორი_პასვორდი:
+        print("პაროლი არასწორია")
+        password = input("შეიყვანე პაროლი")
+    print("სწორია!")
+
+# ---------------------------------------------
+
+fruits = ["apple", "banana", "apple", "orange", "apple", "grape"]
+print("ვაშლი გვხვდება:", fruits.count("apple"), "ჯერ")
+
+# ---------------------------------------------
+
+def multiply(a, b):
+    return a * b
+
+print(multiply(5, 3))
+
+# ---------------------------------------------
+
+animals = ["Lion", "Tiger", "Frog", "Panda", "Deer", "Elephant", "Fox"]
+print(animals[1])
+print(animals[2])
+print(animals[-5])
+print(animals[-6])
+
+
