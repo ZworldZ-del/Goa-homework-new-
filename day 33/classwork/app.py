9095ece64472022a27493d698e2aee2e46cac2be
nikoloz chagelishvili <chagelishvilinikolozi@gmail.com>
2025-10-25 20:47:51 +0400

    second commit

---
+++ day 33/classwork/app.py
@@ -0,0 +1,28 @@
+def my_fuction(f):    
+    return 5/9 * (f - 32)
+    return "wizzard new life is Sumed_Calculated"
+
+print(my_fuction(212))  
+print(my_fuction(32))   
+
+
+
+# 2)sum all parameters <3
+# def calculate_damage(opening_attack, core_damage, finishing_move):
+    
+# ---> "wizzard new life is SUMED_CALCULATED"
+# calculate_damage(10, 20, 30)
+name = 'nikolozi'
+age = 12
+profession = "niggering"
+
+new_result = f"{name} არის {age} წლის და არის {profession}"
+print(new_result)
+
+def calculate_damage(f ,opening_attack, core_damage, finishing_move):
+
+    summed_calculated = opening_attack + core_damage + finishing_move
+
+    return f"wizzard new life is  {summed_calculated}"
+
+print(calculate_damage(10, 20, 30))
---
+++ day 33/homework/app.py
@@ -0,0 +1,19 @@
+def greet(name="chagela"):
+    print("hello,", name)
+
+def sum(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print(sum(5, 3))
+print(substract(10, 4))
+print(multiply(6, 7))
+print(divide(20, 5))
